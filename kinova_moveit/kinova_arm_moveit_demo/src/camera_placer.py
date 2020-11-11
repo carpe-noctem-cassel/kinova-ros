329c76ca85ebfa4e12dd466a366422c9dca6b798
StefanSchmelz <stefan_schmelz@gmx.net>
2020-11-11 17:35:37 +0100

    Tried to implement quarternion offset for the camera rotation.

--- kinova_moveit/kinova_arm_moveit_demo/src/camera_placer.py
+++ kinova_moveit/kinova_arm_moveit_demo/src/camera_placer.py
@@ -9,6 +9,16 @@ from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SetModelState
 
 
+def euler_to_quaternion(roll, pitch, yaw):
+
+        qx = math.sin(roll/2) * math.cos(pitch/2) * math.cos(yaw/2) - math.cos(roll/2) * math.sin(pitch/2) * math.sin(yaw/2)
+        qy = math.cos(roll/2) * math.sin(pitch/2) * math.cos(yaw/2) + math.sin(roll/2) * math.cos(pitch/2) * math.sin(yaw/2)
+        qz = math.cos(roll/2) * math.cos(pitch/2) * math.sin(yaw/2) - math.sin(roll/2) * math.sin(pitch/2) * math.cos(yaw/2)
+        qw = math.cos(roll/2) * math.cos(pitch/2) * math.cos(yaw/2) + math.sin(roll/2) * math.sin(pitch/2) * math.sin(yaw/2)
+
+        return [qx, qy, qz, qw]
+
+
 if __name__ == '__main__':
     print "Hello World"
     rospy.init_node('camera_tf_listener')
@@ -16,12 +26,15 @@ if __name__ == '__main__':
     listener = tf.TransformListener()
     set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 
-    rate = rospy.Rate(10.0)
+    rate = rospy.Rate(100.0)
+
+    # math.pi / -2     math.pi / 2    math.pi / -4
+    transform_quaternion = euler_to_quaternion(0, 0, (math.pi / -15.0))
 
     rospy.wait_for_service('/gazebo/set_model_state')
     while not rospy.is_shutdown():
         try:
-            (trans,rot) = listener.lookupTransform('/j2n6s300_link_4', '/root', rospy.Time(0))
+            (trans,rot) = listener.lookupTransform('/root', '/j2n6s300_link_4', rospy.Time(0))
         except tf.LookupException:
           print "No lookup possible"
           continue
@@ -37,10 +50,10 @@ if __name__ == '__main__':
         state_msg.pose.position.x = trans[0]
         state_msg.pose.position.y = trans[1]
         state_msg.pose.position.z = trans[2]
-        state_msg.pose.orientation.x = rot[0]
-        state_msg.pose.orientation.y = rot[1]
-        state_msg.pose.orientation.z = rot[2]
-        state_msg.pose.orientation.w = rot[3]
+        state_msg.pose.orientation.x = rot[0] * transform_quaternion[0]
+        state_msg.pose.orientation.y = rot[1] * transform_quaternion[1]
+        state_msg.pose.orientation.z = rot[2] * transform_quaternion[2]
+        state_msg.pose.orientation.w = rot[3] * transform_quaternion[3]
         print("Name:{}: {} {} {} | {} {} {} {}".format(state_msg.model_name,
         state_msg.pose.position.x,
         state_msg.pose.position.y,
